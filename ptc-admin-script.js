04bb918c28d02df53a183bedc04e16a1b6cf291e
Ibrahim Monir <54437935+ibrahim-monir@users.noreply.github.com>
2026-03-08 12:11:41 +0600

    Update ptc-admin-script.js

--- ptc-admin-script.js
+++ ptc-admin-script.js
@@ -1,4 +1,4 @@
- // Updated only this section to add SKU after title and size
+// Updated only this section to add SKU after title and size
             let orderItems = '';
 
             orderData?.items?.forEach(function (item) {
@@ -12,3 +12,15 @@
                 </li>`;
             });
 
+            orderTotalItemsDom.html(orderData?.total_items);
+            orderItemsDom.html(orderItems);
+
+            let defaultCityId = orderData?.shipping?.city_id ?? orderData?.billing?.city_id
+            let defaultZoneId = orderData?.shipping?.zone_id ?? orderData?.billing?.city_id
+            let defaultAreaId = orderData?.shipping?.area_id ?? orderData?.billing?.city_id
+            await populateCityZoneArea(defaultCityId, defaultZoneId, defaultAreaId);
+            await populateStores();
+
+            // Autofill item description with product name + quantity, each on a new line
+            const productDescriptions = orderData?.items?.map(item => `${item.name} x${item.quantity} - ${item.sku ? item.sku : 'No SKU'}`).join('\n');
+            itemDescriptionInput.val(productDescriptions);
